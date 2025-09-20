c2777f1b7251aa146735c7e6c8e58a94802bf7d8
nithish-sakthivel-1112 <nithish0829@gmail.com>
2025-09-21 00:18:16 +0530

    feat: Added the IAC for the infrastructure

---
+++ terraform/s3.tf
@@ -0,0 +1,45 @@
+resource "aws_s3_bucket" "hosting_bucket" {
+  bucket = var.s3_bucket_name
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.hosting_bucket.id
+  key          = "index.html"
+  source       = "../dist/index.html"
+  etag         = filemd5("../dist/index.html")
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "index_js" {
+  bucket       = aws_s3_bucket.hosting_bucket.id
+  key          = "assets/index-DuGuS2xc.js"
+  source       = "../dist/assets/index-DuGuS2xc.js"
+  etag         = filemd5("../dist/assets/index-DuGuS2xc.js")
+  content_type = "application/javascript"
+}
+
+resource "aws_s3_object" "index_css" {
+  bucket       = aws_s3_bucket.hosting_bucket.id
+  key          = "assets/index-BbwWs9Rm.css"
+  source       = "../dist/assets/index-BbwWs9Rm.css"
+  etag         = filemd5("../dist/assets/index-BbwWs9Rm.css")
+  content_type = "text/css"
+}
+
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.hosting_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "s3:GetObject"
+        Effect   = "Allow"
+        Resource = "${aws_s3_bucket.hosting_bucket.arn}/*"
+        Principal = {
+          CanonicalUser = aws_cloudfront_origin_access_identity.origin_access_identity.s3_canonical_user_id
+        }
+      }
+    ]
+  })
+}
